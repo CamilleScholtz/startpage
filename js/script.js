9d4d4cc1bd37f8a4ae35437859720ae5e4c26724
onodera-punpun <onodera@openmailbox.org>
2015-06-10 21:36:30 +0200

    dd

--- js/script.js
+++ js/script.js
@@ -1,32 +1,20 @@
 $(document).ready(function(){
 
+	$('.feedrice').hide({effect: 'clip'});
+	$('.feedsyria').hide({effect: 'clip'});
+	$('.feedkorea').hide({effect: 'clip'});
+	$('.feedukraine').hide({effect: 'clip'});
+
 	$('.rice').on('click', function () {
 		$('.rice').attr('style', 'font-weight: 600 !important');
 		$('.syria').attr('style', 'font-weight: 500 !important');
 		$('.korea').attr('style', 'font-weight: 500 !important');
 		$('.ukraine').attr('style', 'font-weight: 500 !important');
 
-		$('.feedrice').animate({
-			width: '550px',
-			height: '350px',
-			opacity: '1'
-		});
-		$('.feedsyria').animate({
-			width: '30px',
-			height: '0px',
-			opacity: '0'
-		});
-		$('.feedkorea').animate({
-			width: '30px',
-			height: '0px',
-			opacity: '0'
-		});
-		$('.feedukraine').animate({
-			width: '30px',
-			height: '0px',
-			opacity: '0'
-		});
-
+		$('.feedrice').show({effect: 'clip'});
+		$('.feedsyria').hide({effect: 'clip'});
+		$('.feedkorea').hide({effect: 'clip'});
+		$('.feedukraine').hide({effect: 'clip'});
 	});
 
 	$('.syria').on('click', function () {
@@ -35,26 +23,10 @@ $(document).ready(function(){
 		$('.korea').attr('style', 'font-weight: 500 !important');
 		$('.ukraine').attr('style', 'font-weight: 500 !important');
 
-		$('.feedrice').animate({
-			width: '30px',
-			height: '0px',
-			opacity: '0'
-		});
-		$('.feedsyria').animate({
-			width: '550px',
-			height: '350px',
-			opacity: '1'
-		});
-		$('.feedkorea').animate({
-			width: '30px',
-			height: '0px',
-			opacity: '0'
-		});
-		$('.feedukraine').animate({
-			width: '30px',
-			height: '0px',
-			opacity: '0'
-		});
+		$('.feedrice').hide({effect: 'clip'});
+		$('.feedsyria').show({effect: 'clip'});
+		$('.feedkorea').hide({effect: 'clip'});
+		$('.feedukraine').hide({effect: 'clip'});
 	});
 
 	$('.korea').on('click', function () {
@@ -63,27 +35,10 @@ $(document).ready(function(){
 		$('.korea').attr('style', 'font-weight: 600 !important');
 		$('.ukraine').attr('style', 'font-weight: 500 !important');
 
-
-		$('.feedrice').animate({
-			width: '30px',
-			height: '0px',
-			opacity: '0'
-		});
-		$('.feedsyria').animate({
-			width: '30px',
-			height: '0px',
-			opacity: '0'
-		});
-		$('.feedkorea').animate({
-			width: '550px',
-			height: '350px',
-			opacity: '1'
-		});
-		$('.feedukraine').animate({
-			width: '30px',
-			height: '0px',
-			opacity: '0'
-		});
+		$('.feedrice').hide({effect: 'clip'});
+		$('.feedsyria').hide({effect: 'clip'});
+		$('.feedkorea').show({effect: 'clip'});
+		$('.feedukraine').hide({effect: 'clip'});
 	});
 
 	$('.ukraine').on('click', function () {
@@ -92,27 +47,10 @@ $(document).ready(function(){
 		$('.korea').attr('style', 'font-weight: 500 !important');
 		$('.ukraine').attr('style', 'font-weight: 600 !important');
 
-
-		$('.feedrice').animate({
-			width: '30px',
-			height: '0px',
-			opacity: '0'
-		});
-		$('.feedsyria').animate({
-			width: '30px',
-			height: '0px',
-			opacity: '0'
-		});
-		$('.feedkorea').animate({
-			width: '30px',
-			height: '0px',
-			opacity: '0'
-		});
-		$('.feedukraine').animate({
-			width: '550px',
-			height: '350px',
-			opacity: '1'
-		});
+		$('.feedrice').hide({effect: 'clip'});
+		$('.feedsyria').hide({effect: 'clip'});
+		$('.feedkorea').hide({effect: 'clip'});
+		$('.feedukraine').show({effect: 'clip'});
 	});
 
 });
